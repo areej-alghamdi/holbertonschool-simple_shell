dcf6628002e71093bf8f462fd2b69b10ccc6148b
areej-alghamdi <areejaa12@gmail.com>
2026-05-21 22:14:05 +0300

    Fix cd: silently succeed when no arg and HOME unset, matching sh

--- builtins1.c
+++ builtins1.c
@@ -102,6 +102,8 @@ int handle_cd(char **args, char **av, int *last_status)
 	else
 		target = args[1];
 	if (target == NULL || chdir(target) != 0)
+	if (args[1] == NULL && home == NULL)
+		return (1);
 	{
 		
 	    write(STDERR_FILENO, av[0], _strlen(av[0]));
