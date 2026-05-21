2fa8f6e4cc0fb78ecf7950b819700c64798e1f1a
Abeer Ali. <abeer.ali.alsaiari1@gmail.com>
2026-05-21 20:45:04 +0300

    Add _itoa and expand_variables function prototypes

--- shell.h
+++ shell.h
@@ -8,35 +8,30 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <signal.h>
-#include <string.h>
 
 extern char **environ;
-
 int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_strdup(char *str);
-
 int is_delim(char c, const char *delim);
 char *_strtok(char *str, const char *delim);
 char *_getenv(const char *name);
-
 int _atoi_exit(char *s, int *error);
 int handle_builtins(char **args, char *line, char **av, int last_status);
-
 int match_env_name(char *env_var, char *name);
 int _setenv(char *name, char *value);
 int _unsetenv(char *name);
 int handle_env_builtins(char **args);
-
 char *get_location(char *command);
 int execute_command(char **args, char **av, char *line, int *last_status);
 void parse_command(char *line, char **args);
-
 void init_env(void);
 void free_env(void);
 void sigint_handler(int sig);
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+char *_itoa(int num);
+void expand_variables(char **args, int last_status);
 
 #endif
